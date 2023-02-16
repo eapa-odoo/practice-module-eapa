f611326f4c6cbe4a82383b51db4ad4ea212257fa
Earth Patel <eapa@odoo.com>
2023-02-16 18:57:46 +0530

    additional fields in showroom_vehicle model

--- showroom/models/showroom_vehicle.py
+++ showroom/models/showroom_vehicle.py
@@ -10,6 +10,24 @@ class ShowroomVehicle(models.Model):
     veh_cost = fields.Integer(string='Cost')
     veh_model_id = fields.Many2one('showroom.vehicle.model',required=True, string='Model')
     veh_type_id = fields.Many2one('showroom.vehicle.type',required=True)
+    veh_image = fields.Image()
 
     veh_model_year = fields.Integer()
-    veh_transmission = fields.Selection([('M','Manual'),('A','Automatic')])
+    veh_transmission = fields.Selection(
+        selection=[('M','Manual'),('A','Automatic')]
+    )
+
+    veh_horsepower = fields.Integer()
+    veh_power = fields.Integer()
+    veh_fuel_type = fields.Selection(
+        selection=[('diesel','Diesel'),
+                   ('gasoline','Gasoline'),
+                   ('full hybrid','Full Hybrid'),
+                   ('plug-in hybrid diesel','Plug-in Hybrid Diesel'),
+                   ('plug-in hybrid gasoline','Plug-in Hybrid Gasoline'),
+                   ('cng','CNG'),
+                   ('lpg','LPG'),
+                   ('electric','Electric')]
+    )
+    veh_emissions = fields.Float()
+
